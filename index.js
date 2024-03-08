7bf0ab10d091ec143411cccb8a614d758cde4a68
Amer Mekic <amerm3kic@gmail.com>
2024-03-08 17:00:01 +0100

    Post, view, update & delete implemented

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 const blogs = [];
-var initialized = false;
 
 app.get("/", (req, res) => {
     res.render("index.ejs",
@@ -19,7 +20,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/post-blog", (req, res) => {
-    res.render("blogform.ejs");
+    res.render("blogform.ejs", 
+    {
+        existingTitle:null,
+        existingContent:null
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -34,14 +39,12 @@ app.post("/blogpost", (req, res) => {
     var date = new Date();
     var month1 = date.toLocaleString('en-US', { month: 'long' });
     var day1 = date.getDate();
-
     var blogData = {
         title:req.body.blogTitle,
         month:month1,
         day:day1,
         blogText: req.body.blogContent
     }
-
     blogs.push(blogData);
     res.redirect("/");
 });
@@ -66,12 +69,30 @@ app.get('/viewBlog/:blogID', (req, res) => {
       }
 });
 
-app.post('/deleteitems/:blog', (req, res) => {
-    const blogIndex = req.params.blog;
-    console.log(blogIndex);
+app.post('/deleteitems/:blogID', (req, res) => {
+    const blogIndex = req.params.blogID;
     if (blogIndex !== -1) {
       blogs.splice(blogIndex, 1);
     };
+    res.redirect("/");
+});
+
+app.get("/update/:blogID", (req, res) => {
+    var blogIndex = req.params.blogID;
+    var title = blogs[blogIndex].title;
+    var content = blogs[blogIndex].blogText;
+    res.render("updateBlog.ejs" , 
+    {
+        blogIndex:blogIndex,
+        existingTitle: title,
+        existingContent:content,
+    });
+});
+
+app.post("/blog-update/:blogIndex" , (req,res) => {
+    var blogI = req.params.blogIndex;
+    blogs[blogI].title = req.body.blogTitleUpdated;
+    blogs[blogI].blogText = req.body.blogContentUpdated;
     res.redirect("/");
 });
 
